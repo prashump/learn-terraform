36a187d035ac27a1795e13598c4cd3ed8612f7cb
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-06-28 17:04:31 +0530

    learn devops

--- variables/main.tf
+++ variables/main.tf
@@ -5,3 +5,52 @@ variable "x" {
 output "x" {
   value = var.x
 }
+
+# string, number, boolean
+# string requires double quotes
+
+variable "str" {
+  default = "Hello"
+}
+
+# variable "num" {
+#   default = 20
+# }
+#
+# variable "mybool" {
+#   default = true
+# }
+
+output "string" {
+  value = "${var.str}, steve"
+}
+
+variable "a" {
+  default = "this is normal variable"
+}
+
+variable "b" {
+  default = [
+    1, 2, "abc", false
+  ]
+}
+
+variable "c" {
+  default = {
+    a = 10
+    b = 20
+    z = "mps"
+  }
+}
+
+output "b1" {
+  value = var.b[0]
+}
+
+output "c1" {
+  value = var.c["z"]
+}
+
+output "a1" {
+  value = var.a
+}
