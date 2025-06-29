bcb8669dfbbaf2d70d6d7c89dba8960596c618e2
Prasanth Reddy <prasanthreddy1792@gmail.com>
2025-06-29 12:06:30 +0530

    learn devops

--- data/main.tf
+++ data/main.tf
@@ -1,5 +1,10 @@
 # data block is to get the info from provider or existing resources
 
+provider "azurerm" {
+  features {}
+  subscription_id = "fce3d656-0949-412a-9e49-d5c96a4783c9"
+}
+
 data "azurerm_resource_group" "example" {
   name = "project-setup"
 }
